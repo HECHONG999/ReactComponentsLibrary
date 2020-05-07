ec706ca307512df8cc505e5223f921bba14559a3
HECHONG999 <1259796851@qq.com>
2020-05-07 18:53:22 +0800

    commit

--- reactcomponent/src/App.js
+++ reactcomponent/src/App.js
@@ -4,7 +4,6 @@ import usePageStudent from "./myHooks/usePageStudent"
 function Test() {
   const [page, setPage] = useState(1)
   const resp = usePageStudent(page, 5);
-  console.log(resp, ")))))))))")
   if( resp.length != 0) {
     const list = resp.findByPage.map( it => {
       return <li key={it.id}>姓名: {it.name}</li>
--- reactcomponent/src/index.js
+++ reactcomponent/src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import PagerTest from "./components/page/PagerTest";
-// import ThreeLayout from "./components/ThreeLayout"
-// import Test from "./components/ThreeLayout/Test"
-// import StudentContainer from "./components/studentContainer"
 
 import App from "./App"
 ReactDOM.render(
