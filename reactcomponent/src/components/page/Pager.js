aeddd33eefad09b9fa3b2e7c37b5d40211879786
HECHONG999 <49875337+HECHONG999@users.noreply.github.com>
2021-03-16 21:38:38 +0800

    Pager

--- reactcomponent/src/components/page/Pager.js
+++ reactcomponent/src/components/page/Pager.js
@@ -1,72 +1,72 @@
 import React from 'react';
-import "./Pager.css"
 /**
  * 分页组件
- * 属性：
- * 1. current：初始页码
- * 2. total：总数据量
- * 3. limit：页容量，每页显示的数据量
- * 4. panelNumber：数字页码最多显示多少个
- * 5. onPageChange：当页码改变的事件
+ * 1.current：当前页码
+ * 2.totle：数据总量
+ * 3.limit: 页容量
+ * 4.panelNumber： 数字页码最多显示几个
+ * 5.onPageChange  当前页码改变的事件
+ * @param {*} props 
+ * @returns 
  */
 export default function Pager(props) {
-    let pageNumber = getTotalPage(props);
-    let min = getMin(props.current, props);
-    let max = getMax(min, pageNumber, props)
-    let spanArr = []
-    for(let i = min; i < max ; i ++) {
-        spanArr.push(<span onClick={() => {toPage(i, pageNumber, props)}} className={ i === props.current ? "active item" : "item" } key={i}>{i}</span>)
+    let pageNumber = getPageNumber(props);
+    let min = getMinNumber(props);
+    let max = getMaxNumber(min, pageNumber, props);
+    let numbers = []
+    for( let i = min; i < max + 1; i ++) {
+        numbers.push(<span onClick={() => {toPage(i, props)}} className={props.current == i ? "item active" : "item"}>{i}</span>)
     }
     return (
         <>
-            <span className={ props.current === 1 ? "item disable" : "item "} onClick={() => { toPage(1, pageNumber, props)} }>首页</span>
-            <span className={ props.current === 1 ? "item disable" : "item "} onClick={ () => {toPage(props.current - 1, pageNumber, props)} }>上一页</span>
-            {/* 数字页码 */}
-            {spanArr}
-            <span className={ props.current === pageNumber ? "item disable" : "item "} onClick={ () => {toPage(props.current + 1, pageNumber, props)} }>下一页</span>
-            <span className={ props.current === pageNumber ? "item disable" : "item "} onClick={ () => {toPage(pageNumber, pageNumber, props)} }>尾页</span>
+            <span onClick={() => {toPage(1,props)}}
+                className={props.current == 1 ? "item disabled" : "item"}>首页</span>
+            <span onClick={() => {toPage(props.current - 1,props)}}
+                className={props.current == 1 ? "item disabled" : "item"}
+            >上一页</span>
+            {numbers}
+            <span onClick={() => {toPage(props.current + 1,props)}}
+                className={props.current == pageNumber ? "item disabled" : "item"}
+            >下一页</span>
+            <span onClick={() => {toPage(pageNumber,props)}}
+                className={props.current == pageNumber ? "item disabled" : "item"}
+             >尾页</span>
+             <span>{props.current}</span>/<span>{props.panelNumber}</span>
         </>
     )
 }
 
-function getMin(current, props) {
-    let min = current - Math.floor( props.panolNumber - 1 ) / 2;
-    if( min < 1 ) {
-        min = 1
-    }
-    return min
-}
-
-function getMax(min, pageNumber, props) {
-    let max  =  min + props.panolNumber ;
+function getMaxNumber(min, pageNumber,props) {
+    let max = min + props.panelNumber  - 1;
     if(max >= pageNumber) {
-        max = pageNumber
+        max = pageNumber;
     }
     return max
 }
-
 /**
- * 跳转到指定页面
- * @param {*} target 
+ * 得到最小页码
  * @param {*} props 
+ * @returns 
  */
-function toPage(target, pageNumber, props) {
-   
-    // if(target <= 0) {
-    //     return 
-    // }
-    // if (target >= pageNumber ) {
-    //     return
-    // }
-    if( target == props.current) { return}
-   props.onPageChange && props.onPageChange(target)
+function getMinNumber(props) {
+   let min =  props.current - Math.floor(props.panelNumber / 2)
+   if(min <= 1) {
+       min = 1
+   } 
+   return min;
+}
+/**
+ * 计算页码总数
+ * @param {*} props 
+ * @returns 
+ */
+function getPageNumber(props) {
+    return Math.ceil( props.total / props.limit);
 }
 
-/**
- * 计算出总页数
- * @param {*} props 
- */
-function getTotalPage(props) {
-    return Math.ceil( props.total / props.limit )
+function toPage(target, props) {
+    if(props.current == target) {
+        return;
+    }
+    props.onPageChange && props.onPageChange(target)
 }
-
--- reactcomponent/src/components/page/PagerTest.js
+++ reactcomponent/src/components/page/PagerTest.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Pager from "./Pager"
-
 export default class PagerTest extends Component {
     state = {
-        current: 4,
-        total: 100,
+        current: 2,
         limit: 10,
-        panolNumber: 5
+        total: 100,
+        panelNumber: 5
     }
-    /**
-     * 1. target 跳转到当前展示的页面
-     * 2. props 组件的属性
-     */
-    toPage = (newPage) => {
-        console.log(newPage)
+    handlePageChange = (newPage) => {
         this.setState({
             current: newPage
         })
     }
-
     render() {
         return (
-           <Pager {...this.state} onChange={this.toPage} />
+            <>
+              <Pager {...this.state} onPageChange={this.handlePageChange}></Pager>  
+            </>
         )
     }
 }
